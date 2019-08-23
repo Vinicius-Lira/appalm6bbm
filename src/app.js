27f979ff8aa6457173e69f35a603100523fb0d56
Vinicius Lira <viniciusklira@gmail.com>
2019-08-23 18:02:09 -0300

    sdvsd

---
+++ src/app.js
@@ -0,0 +1,22 @@
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const router = express.Router();
+
+// Carrega as Rotas
+const index = require('./routes/index');
+const teste = require('./routes/teste');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+app.use('/', index);
+app.use('/teste', teste);
+
+
+module.exports = app;
